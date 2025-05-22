53043ceaf50527359a7408e6d096d102204b85b9
Ricardo <ricardo.carriel@aluno.ifsp.edu.br>
2025-05-22 17:00:04 +0000

    gitignore e settings

--- code/setup/setup/settings.py
+++ code/setup/setup/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',  # Ou postgresql-psycopg2
+                'NAME': 'pousada',  # Substituir por seu nome
+                'USER': 'postgres',  # Substituir por seu nome
+                'PASSWORD': 'abc123',  # Substituir por sua senha
+                'HOST': 'localhost',
+                'PORT': '5432',
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
