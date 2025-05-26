9365f99540b9ee5f10cafd74654181ab01c10136
ricardo <ricardo@git.objectdata.com.br>
2025-05-25 23:17:42 -0300

    ajustes layout base, urls padronizadas, tema de cores do usuario e arquivos estaticos

--- code/setup/apps/hospede/urls.py
+++ code/setup/apps/hospede/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'hospede'
+
 urlpatterns = [
     path('', views.hospede_list, name='hospede_list'),
-    path('criar/', views.hospede_create, name='hospede_create'),
-    path('editar/<str:cpf>/', views.hospede_update, name='hospede_update'),
-    path('deletar/<str:cpf>/', views.hospede_delete, name='hospede_delete'),
+    path('novo/', views.hospede_create, name='hospede_create'),
+    path('<str:cpf>/', views.hospede_detail, name='hospede_detail'),
+    path('<str:cpf>/editar/', views.hospede_update, name='hospede_update'),
+    path('<str:cpf>/excluir/', views.hospede_delete, name='hospede_delete'),
     path('buscar/', views.hospede_search, name='hospede_search'),
-    path('detalhe/<str:cpf>/', views.hospede_detail, name='hospede_detail'),
 ]
--- code/setup/setup/urls.py
+++ code/setup/setup/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: render(request, 'index.html'), name='index'),
     path('quartos/', include('apps.quarto.urls')),
-    path('hospedes/', include('apps.hospede.urls')),
+    path('hospedes/', include('apps.hospede.urls', namespace='hospede')),
 ]
