106f6a2dca3a44815d216e028ce599c9c0f27fb9
Rafael Teixeira Fonseca <rafaelteixeirafonseca@gmail.com>
2025-06-07 09:50:30 -0300

    Importando funcoes de editar e excluir quartos

--- code/setup/apps/quarto/urls.py
+++ code/setup/apps/quarto/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from apps.quarto.views import quartos, index, adicionar_quarto, tipos_quarto
+from apps.quarto.views import (
+    quartos, index, adicionar_quarto, tipos_quarto, editar_quarto, excluir_quarto,
+)
 
 app_name = 'quarto'
 
@@ -7,5 +9,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('quartos/', quartos, name='quartos'),
     path('quartos/adicionar/', adicionar_quarto, name='adicionar_quarto'),
+    path('quartos/editar/<int:quarto_id>/', editar_quarto, name='editar_quarto'),
+    path('quartos/excluir/<int:quarto_id>/', excluir_quarto, name='excluir_quarto'),
     path('quartos/tipos/', tipos_quarto, name='tipos_quarto'),
 ]
