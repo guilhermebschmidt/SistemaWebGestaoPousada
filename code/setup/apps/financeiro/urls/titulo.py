23915d3ae1944978cd9df4d1e21f321a8a001242
ricardo <ricardo@git.objectdata.com.br>
2025-10-01 15:56:16 -0300

    botão para marcar a reserva como paga e cofirmar a reserva"

--- code/setup/apps/financeiro/urls/titulo.py
+++ code/setup/apps/financeiro/urls/titulo.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.list_titulos, name='list'),
     path('novo/', views.form, name='form'),
     path('editar/<int:pk>/', views.form, name='update'),
+    path('titulos/<int:pk>/marcar-pago/', views.marcar_pago, name='marcar_pago'),
 ]
