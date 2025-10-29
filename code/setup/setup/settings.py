fa83a4df3c5ddcc13161bd66c4a03f9e1aa75907
Kelly Radchelle <radchelle.kelly@gmail.com>
2025-10-28 22:00:40 -0300

    Teste de alteração

--- code/setup/setup/settings.py
+++ code/setup/setup/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pousada',
@@ -97,16 +97,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
